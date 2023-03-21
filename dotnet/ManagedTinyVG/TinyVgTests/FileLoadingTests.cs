fa6f045d2f8b8ffe7ec040cdc0cdb8a8de630697
Iain Ballard <iain.ballard@ewater.services>
2023-03-21 14:58:06 +0000

    example file loading tests

--- dotnet/ManagedTinyVG/TinyVgTests/FileLoadingTests.cs
+++ dotnet/ManagedTinyVG/TinyVgTests/FileLoadingTests.cs
@@ -40,4 +40,52 @@ public class FileLoadingTests
         Assert.That(document.Height, Is.EqualTo(768), "height");
         Assert.That(document.Width, Is.EqualTo(400), "width");
     }
+    
+    [Test]
+    public void load_everything_32()
+    {
+        var byteArray = File.ReadAllBytes("examples/everything-32.tvg"); //400 x 768
+        
+        var document = TvgLoad.FromByteArray(byteArray);
+        
+        Assert.That(document, Is.Not.Null);
+        Assert.That(document.Height, Is.EqualTo(768), "height");
+        Assert.That(document.Width, Is.EqualTo(400), "width");
+    }
+    
+    [Test]
+    public void load_shield_8()
+    {
+        var byteArray = File.ReadAllBytes("examples/shield-8.tvg");
+        
+        var document = TvgLoad.FromByteArray(byteArray);
+        
+        Assert.That(document, Is.Not.Null);
+        Assert.That(document.Height, Is.EqualTo(24), "height");
+        Assert.That(document.Width, Is.EqualTo(24), "width");
+    }
+    
+    [Test]
+    public void load_shield_16()
+    {
+        var byteArray = File.ReadAllBytes("examples/shield-16.tvg");
+        
+        var document = TvgLoad.FromByteArray(byteArray);
+        
+        Assert.That(document, Is.Not.Null);
+        Assert.That(document.Height, Is.EqualTo(24), "height");
+        Assert.That(document.Width, Is.EqualTo(24), "width");
+    }
+    
+    [Test]
+    public void load_shield_32()
+    {
+        var byteArray = File.ReadAllBytes("examples/shield-32.tvg");
+        
+        var document = TvgLoad.FromByteArray(byteArray);
+        
+        Assert.That(document, Is.Not.Null);
+        Assert.That(document.Height, Is.EqualTo(24), "height");
+        Assert.That(document.Width, Is.EqualTo(24), "width");
+    }
 }
